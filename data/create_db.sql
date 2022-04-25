e69c7b8924de439bb1432604514b4acb58cd32e5
Revertron <rev@revertron.com>
2022-04-25 17:29:01 +0200

    Optimized database layer.

--- data/create_db.sql
+++ data/create_db.sql
@@ -2,7 +2,8 @@ CREATE TABLE data (
     'client' VARCHAR(16),
     'hostname' VARCHAR(32),
     'blacklist' INT,
-    'ip' VARCHAR(40)
+    'ip' VARCHAR(40),
+    'until' INT
 );
 
 CREATE INDEX client_host ON data (client, hostname);
